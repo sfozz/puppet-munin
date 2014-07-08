33023bc9dc33b5fb68a5bf26061529ef7b77fb1c
Steve Foster <s.p.foster@leeds.ac.uk>
2014-07-08 15:24:41 +0100

    Add missing paramters and tidy

--- manifests/params.pp
+++ manifests/params.pp
@@ -7,8 +7,12 @@ class munin::params {
   
   case $::osfamily {
     redhat: {
+      $user           = 'root'
+      $group          = 'root'
+      $log_file       = '/var/log/munin/munin-node.log'
+      $client_service = 'munin-node'
       $client_package = 'munin-node'
-      $client_conf = '/etc/munin/munin-node.conf'
+      $client_conf    = '/etc/munin/munin-node.conf'
       $client_plugins = [ 'df', 'cpu', 'interrupts', 'load', 'memory',
                           'netstat', 'open_files', 'processes', 'swap',
                           'uptime', 'users', 'vmstat','df_abs', 'forks',
@@ -16,8 +20,12 @@ class munin::params {
                           'diskstats', 'proc_pri', 'threads', ]
     }
     debian: {
+      $user           = 'root'
+      $group          = '0'
+      $log_file       = '/var/log/munin/munin-node.log'
+      $client_service = 'munin-node'
       $client_package = [ 'munin-node', 'iproute' ]
-      $client_conf = '/etc/munin/munin-node.conf'
+      $client_conf    = '/etc/munin/munin-node.conf'
       $client_plugins = [ 'df', 'cpu', 'interrupts', 'load', 'memory',
                           'netstat', 'open_files', 'processes', 'swap',
                           'uptime', 'users', 'vmstat','df_abs', 'forks',
