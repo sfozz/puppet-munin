d4d9241ab153bbee6f102605096091020b196317
Steve Foster <s.p.foster@leeds.ac.uk>
2014-07-08 15:22:49 +0100

    Tidy parameters & add dependency
    Parameters needed tidying and some missing ones added.
    
    Also added dependency for munin::client::register so it happens after the service

--- manifests/client.pp
+++ manifests/client.pp
@@ -4,19 +4,23 @@
 
 # configure a munin node
 class munin::client(
-  $allow                      = [ '127.0.0.1' ],
-  $host                       = '*',
-  $host_name                  = $::fqdn,
-  $port                       = '4949',
-  $use_ssh                    = false,
-  $export_tag                 = 'munin',
-  $description                = 'absent',
-  $munin_group                = 'absent',
+  $allow       = [ '127.0.0.1' ],
+  $listen      = '*',
+  $target      = $::fqdn,
+  $port        = '4949',
+  $use_ssh     = false,
+  $export_tag  = 'munin',
+  $description = undef,
+  $munin_group = undef,
+  $config      = [ 'use_node_name yes', 'load.load.warning 5', 'load.load.critical 10' ],
+  $enable      = true,
+  $ensure      = true,
 ) {
 
   class { 'munin::client::install': } ->
   class { 'munin::client::config': } ~>
   class { 'munin::client::service': } ->
+  class { 'munin::client::register': } ->
   Class ['munin::client']
 
   Class ['munin::client::install'] ~> Class ['munin::client::service']
