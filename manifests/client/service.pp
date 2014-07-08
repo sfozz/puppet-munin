cce7d8b7edc8c86559567a9a15ac340f11187d41
Steve Foster <s.p.foster@leeds.ac.uk>
2014-07-08 15:25:15 +0100

    Use correct service name from munin::params

--- manifests/client/service.pp
+++ manifests/client/service.pp
@@ -3,7 +3,7 @@
 #
 class munin::client::service inherits munin::params {
 
-  service { $munin::params::service:
+  service { $munin::params::client_service:
     ensure => $munin::client::ensure,
     enable => $munin::client::enable,
   }
